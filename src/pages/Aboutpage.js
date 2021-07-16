f02ffc757b0497992e2d70e5d6cb4b4b80ca357e
tengfone <phangtf@gmail.com>
2021-07-16 17:17:40 +0800

    added sell section

--- src/pages/Aboutpage.js
+++ src/pages/Aboutpage.js
@@ -5,7 +5,15 @@ function Aboutpage() {
     return (
         <div>
             <h1>About Page</h1>
-            <p>ABOUT PARAGRAPH</p>
+            <p>OpenRiver is a Dapp project for Singapore University of Technology and Design (SUTD) 2021 May Term - 50.037 : Blockchain Technology</p>
+            <p>Done By:</p>
+            <ul>
+                <li>Phang Teng Fone</li>
+                <li>Ryan Gen Zi Qiang</li>
+                <li>Ng Ming Bing</li>
+                <li>Lim Boon Hao Gerald</li>
+            </ul>
+            <p>Inspired by OpenSea.io</p>
         </div>
     )
 }
--- src/pages/Sellpage.js
+++ src/pages/Sellpage.js
@@ -1,11 +1,112 @@
-import React from 'react'
+import { React, useState } from 'react'
 import '../App.css'
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap'
 
 function Sellpage() {
+
+    const [form, setForm] = useState({})
+    const [errors, setErrors] = useState({})
+
+    const setField = (field, value) => {
+        setForm({
+            ...form,
+            [field]: value
+        })
+        // Check and see if errors exist, and remove them from the error object:
+        if (!!errors[field]) setErrors({
+            ...errors,
+            [field]: null
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        // get our new errors
+        const newErrors = findFormErrors()
+        // Conditional logic:
+        if (Object.keys(newErrors).length > 0) {
+            // We got errors!
+            setErrors(newErrors)
+        } else {
+            // No errors! Put any logic here for the form submission!
+            console.log(form)
+            console.log("Submitted")
+        }
+    }
+
+    const findFormErrors = () => {
+        const { name, type, file, price, description } = form
+        const newErrors = {}
+        // name errors
+        if (!name || name === '') newErrors.name = 'Cannot be blank!'
+        else if (name.length > 30) newErrors.name = 'name is too long!'
+        // type errors
+        if (!type || type === '') newErrors.type = 'Select a Type'
+        // file errors
+        if (!file || file === '') {
+            newErrors.file = 'Upload an image!'
+        }
+        // price errors
+        if (!price || price < 0) newErrors.price = 'Must be more than 0 ETH'
+        // description errors
+        if (!description || description === '') newErrors.description = 'Description be blank!'
+        return newErrors
+    }
+
     return (
-        <div>
-            <h1>Sell Page</h1>
-            <p>Sell PARAGRAPH</p>
+        <div className='d-flex flex-column align-items-center'>
+            <h1>Sell</h1>
+            <Form style={{ width: '300px' }}>
+                <Form.Group>
+                    <Form.Label>Item Name</Form.Label>
+                    <Form.Control
+                        type='text'
+                        onChange={e => setField('name', e.target.value)}
+                        isInvalid={!!errors.name}
+                    />
+                    <Form.Control.Feedback type='invalid'>{errors.name}</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Type</Form.Label>
+                    <Form.Control
+                        as='select'
+                        onChange={e => setField('type', e.target.value)}
+                        isInvalid={!!errors.type}
+                    >
+                        <option value=''>Select Type of Digital Asset:</option>
+                        <option value='artwork'>Artwork</option>
+                        <option value='game'>Game Items</option>
+                        <option value='software'>Software</option>
+                        <option value='others'>Others</option>
+                    </Form.Control>
+                    <Form.Control.Feedback type='invalid'>{errors.type}</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group controlId="formFile">
+                    <Form.Label>Upload File Image</Form.Label>
+                    <Form.Control onChange={e => setField('file', e.target.files)} isInvalid={!!errors.file} type="file" />
+                    <Form.Control.Feedback type='invalid'>{errors.file}</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Price (ETH)</Form.Label>
+                    <Form.Control
+                        type='number'
+                        onChange={e => setField('price', e.target.value)}
+                        isInvalid={!!errors.price}
+                    />
+                    <Form.Control.Feedback type='invalid'>{errors.price}</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control
+                        as='textarea'
+                        onChange={e => setField('description', e.target.value)}
+                        isInvalid={!!errors.description}
+                    />
+                    <Form.Control.Feedback type='invalid'>{errors.description}</Form.Control.Feedback>
+                </Form.Group>
+                <Button type='submit' onClick={handleSubmit}>Sell!</Button>
+            </Form>
         </div>
     )
 }
