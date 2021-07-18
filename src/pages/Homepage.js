7ceb7cf856b55d00c8e8935231205b6b882a68a2
geraldspacelim <geraldlim6.12@gmail.com>
2021-07-18 21:02:11 +0800

    Ui edits

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Button } from 'react-bootstrap'
 import { useHistory } from "react-router-dom";
 import './Homepage.css'
+import heroImage from '../assets/heroImage.png'
+
+
 
 function Homepage() {
     const history = useHistory();
@@ -17,12 +20,20 @@ function Homepage() {
     }
 
     return (
-        <div>
-            <h1 style={{ textAlign: "left" }}>Discover, collect, and sell extraordinary digital assets</h1>
-            <p style={{ textAlign: "left" }} >on the world's 2nd and smallest digital assets marketplace!</p>
-            <Button onClick={toMarket} variant="primary">Market Place</Button>
-            <Button onClick={toLedger} variant="outline-secondary">Ledger</Button>
-
+        <div className="hero-section">
+            <div className="hero-flex-container">
+                <div className="hero-content">
+                    <h1 style={{ textAlign: "left", fontSize: "48px", marginBottom:"20px"}}>Discover, collect, and sell extraordinary digital assets</h1>
+                    <p style={{ textAlign: "left", fontSize: "18px", marginBottom:"40px" }} >on the world's 2nd and smallest digital assets marketplace!</p>
+                    <div className="hero-form">
+                        <Button onClick={toMarket} variant="primary">Market Place</Button>
+                        <Button onClick={toLedger} variant="outline-secondary" className="ledgerButton">Ledger</Button>
+                    </div>
+                </div>
+                <div className="heroImage">
+                    <img src={heroImage} alt="" width="550" height="515"/>
+                </div>
+            </div>
         </div>
     )
 }
--- src/pages/Marketpage.js
+++ src/pages/Marketpage.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
 import { useHistory } from "react-router-dom";
-import '../App.css'
+import './Marketpage.css'
+import buyImage from '../assets/buyImage.png'
+import sellImage from '../assets/sellImage.png'
 
 function Marketpage() {
     let history = useHistory();
@@ -16,10 +18,22 @@ function Marketpage() {
 
     return (
         <div>
-            <h1>Market Page</h1>
-            <p>Was thinking should be a nice UI page covering full page where split into half, buy and sell</p>
-            <Button onClick={toBuy} variant="primary">Buy</Button>
-            <Button onClick={toSell} variant="outline-secondary">Sell</Button>
+            <h1>Buy and Sell</h1>
+            <p>At Open river, buying and selling art is made simple and secure</p>
+            <div className="market-flex-container">
+                <div className="buySellContainer">
+                    <img id="buyImg" className="marketplaceImg" src={buyImage} alt="" width="477" height="693"/>
+                    <div className="middle">
+                        <div className="buyText" onClick={toBuy}>Buy</div>
+                    </div>
+                </div>
+                <div className="buySellContainer">
+                    <img id="sellImg" className="marketplaceImg" src={sellImage} alt="" width="477" height="693"  onClick={toSell}/>
+                    <div className="middle">
+                        <div className="sellText">Sell</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
