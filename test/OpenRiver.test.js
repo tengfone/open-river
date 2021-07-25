5faafcb4bae2361671098b42539a0bfad1acb763
Ryan <ryangen97@gmail.com>
2021-07-25 20:21:14 +0800

    working version

--- test/OpenRiver.test.js
+++ test/OpenRiver.test.js
@@ -30,14 +30,40 @@ contract('OpenRiver',([deployer,seller,buyer]) =>{
 	  assert.equal(event.owner, seller, 'owner is correct')
 	  assert.equal(event.isPurchased, false, 'purchase is correct')
 	  assert.equal(event.imgHash,'TestHash')
-
-	  //FAILURE: Product have a name
-
 	  await this.OpenRiver.uploadArtwork('', web3.utils.toWei('1', 'Ether'), {from: seller}).should.be.rejected
-
-	  //FAILURE: Product have a price
-
 	  await this.OpenRiver.uploadArtwork('', 0, {from: seller}).should.be.rejected
+
+})
+  	  	it('sell product', async () => {
+  
+  	  		//check seller balance
+  	  		let sellerOldBalance
+  	  		oldBalanceOfSeller =  await web3.eth.getBalance(seller);
+      		oldBalanceOfSeller = new web3.utils.BN(oldBalanceOfSeller);
+      		let oldBalanceofBuyer
+      		oldBalanceofBuyer = await web3.eth.getBalance(buyer);
+      		console.log(oldBalanceofBuyer)
+			result = await this.OpenRiver.purchaseProduct(productCount, {from: buyer, value: web3.utils.toWei('1', 'Ether')});
+			const event = result.logs[0].args;
+			assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct');
+			assert.equal(event.name, 'Testname', 'name is correct');
+			assert.equal(event.price, '1000000000000000000', 'price is correct');
+			assert.equal(event.owner, buyer, 'buyer is correct');
+			assert.equal(event.isPurchased, true, 'purchase is correct');
+
+			//check seller after balance
+			let newBalanceOfSeller;
+			newBalanceOfSeller =  await web3.eth.getBalance(seller);
+			newBalanceOfSeller = new web3.utils.BN(newBalanceOfSeller);
+			let price;
+			price = web3.utils.toWei('1','Ether');
+			price = new web3.utils.BN(price);
+			newBalanceofBuyer = await web3.eth.getBalance(buyer)
+			console.log(newBalanceofBuyer)
+			console.log(oldBalanceOfSeller.toString())
+			console.log(newBalanceOfSeller.toString())
+			const exceptedBalance = oldBalanceOfSeller.add(price);
+      		assert.equal(newBalanceOfSeller.toString(), exceptedBalance.toString());
 
 })
 })
