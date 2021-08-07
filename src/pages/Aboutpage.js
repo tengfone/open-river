d97e8057ed30826e5558e09576e0844dbf90bb1c
geraldspacelim <geraldlim6.12@gmail.com>
2021-08-07 13:20:26 +0800

    about page

--- src/pages/Aboutpage.js
+++ src/pages/Aboutpage.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import './Aboutpage.css'
+import aboutImg from '../assets/aboutImg.png'
 
 function Aboutpage() {
     return (
-        <div className="about-page">
-            <div class="about-section">
-                <h1>About Us Page</h1>
-                      
+        
+        <div class="about-section">
+            <div className="description">
+                <h1>Open River is a decentralised marketplace for buying and selling of unique digital assets. We attribute our inspiration to Open Sea. For everyone, by SUTD.
+</h1>  
             </div>
+            <div className="aboutImg">
+                <img id="aboutImg" src={aboutImg} alt="" width="800" />
+            </div>
+            <div className="footer">
+                © All rights reserved – Ryan Gen, Ng Ming Bing, Gerald Lim, Phang Teng Fone 
+            </div>
+                    
         </div>
         
     )
